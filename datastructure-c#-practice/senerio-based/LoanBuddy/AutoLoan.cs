fe3a5d9a3065c252dc809648f47a5f8813a79d4c
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air-3.local>
2026-01-12 16:53:33 +0530

    adding loan buddy

---
+++ datastructure-c#-practice/senerio-based/LoanBuddy/AutoLoan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    
+        public class AutoLoan : LoanApplication
+        {
+            public AutoLoan(double amount, int tenure)
+                : base(amount, tenure, 9.5) { }
+
+            public override bool ApproveLoan(Applicant applicant)
+            {
+                bool approved = applicant.GetCreditScore() >= 600 &&
+                                applicant.Income >= loanAmount * 0.35;
+
+                SetApprovalStatus(approved);
+                return approved;
+            }
+        }
+    
+
