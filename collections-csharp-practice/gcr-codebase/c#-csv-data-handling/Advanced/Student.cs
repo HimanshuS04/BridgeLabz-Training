2464f4d14ea9e94f13f0a903fa3c5897a3ec8859
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air-3.local>
2026-02-02 16:50:17 +0530

    Revert "adding csv data handling"
    This reverts commit 7d917a3409bdbc3b272547cda549cecaad63943b.

--- collections-csharp-practice/gcr-codebase/c#-csv-data-handling/Advanced/Student.cs
+++
@@ -1,7 +0,0 @@
-public class Student
-{
-    public int Id;
-    public string Name;
-    public int Age;
-    public int Marks;
-}
--- collections-csharp-practice/gcr-codebase/c#-csv-data-handling/Intermediate/Employee.cs
+++
@@ -1,7 +0,0 @@
-public class Employee
-{
-    public int Id;
-    public string Name;
-    public string Department;
-    public double Salary;
-}
